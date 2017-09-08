b5c70e853c1044493c1d41fbe1e0944dfea9775b
rsaldana <rsaldana@lexmark.com>
2017-09-08 17:37:25 +0800

    added user in view

--- srac/views.py
+++ srac/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
@@ -8,4 +9,4 @@ from .models import Location
 @login_required(login_url='/srac/login/')
 def checklist(request, location_hash):
     location = get_object_or_404(Location, pk=location_hash)
-    return HttpResponse('This is a sample')
+    return HttpResponse('Logged in user: {}'.format(request.user))
