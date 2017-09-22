a1ceaed9097797e306ea60890a86e8db803a1aa8
rsaldana <rsaldana@lexmark.com>
2017-09-22 19:19:30 +0800

    optimizations

--- srac/urls.py
+++ srac/urls.py
@@ -1,10 +1,12 @@
-from django.conf.urls import include, url
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 app_name = 'srac'
 urlpatterns = [
-    url('^', include('django.contrib.auth.urls')),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^checklists/(?P<location_hash>[A-Za-z0-9_]+)', views.checklist, name='checklists'),
     url(r'^session/add/(?P<location_hash>[A-Za-z0-9_]+)', views.session_add, name='session.add'),
     url(r'^session/edit/(?P<session_id>[0-9_]+)', views.session_edit, name='session.edit'),
--- srac/views.py
+++ srac/views.py
@@ -25,8 +25,7 @@ def checklist(request, location_hash):
         # check for sessions completed an hour ago
         now = timezone.now()
         one_hour_ago = now - timedelta(hours=1)
-        earlier_sessions_list = Session.objects.filter(check_date__range=(one_hour_ago, now)).filter(
-            is_session_submitted=1)
+        earlier_sessions_list = Session.objects.filter(check_date__range=(one_hour_ago, now)).filter(is_session_submitted=1)
 
         if len(earlier_sessions_list) > 0:
             for earlier_session in earlier_sessions_list:
@@ -48,8 +47,7 @@ def checklist(request, location_hash):
         print("There are existing sessions")
         # check if there is an existing session in the location
         for existing_session in existing_session_set:
-            session_checklist_set = Session_Checklist.objects.filter(session=existing_session)
-            for session_checklist in session_checklist_set:
+            for session_checklist in existing_session.session_checklist_set.all():
                 if session_checklist.location_checklist.location == location:
                     print("There is an existing session for {}: {}".format(location, existing_session.session_id))
                     context = {
@@ -60,19 +58,6 @@ def checklist(request, location_hash):
                     return render(request, 'srac/checklist_prompt.html', context)
 
 
-def session_view(request, session_id):
-    session = get_object_or_404(Session, pk=session_id)
-    session_checklist_set = Session_Checklist.objects.filter(session=session)
-    context = {
-        "checklist_set": session_checklist_set,
-        "session": session
-    }
-    if "success" in request.GET:
-        context["success"] = "true"
-
-    return render(request, 'srac/checklist_view.html', context)
-
-
 def session_add(request, location_hash):
     current_user = get_object_or_404(User, pk=request.user.id)
     location = get_object_or_404(Location, pk=location_hash)
@@ -81,44 +66,24 @@ def session_add(request, location_hash):
         'user': current_user,
     }
 
-    # if form is submitted
-    if request.method == 'POST':
-        session = Session.objects.get(pk=request.POST['session_id'])
-        session_checklist_set = Session_Checklist.objects.filter(session=session)
+    session = Session(
+        check_date=timezone.now(),
+        checker=current_user
+    )
+    session.save()
+    context['session'] = session
 
-        # save changes
-        for session_checklist in session_checklist_set:
-            session_checklist.confirmation = request.POST['confirmation_' + str(session_checklist.session_checklist_id)]
-            session_checklist.remarks = request.POST['remarks_' + str(session_checklist.session_checklist_id)]
-            session_checklist.save()
-
-        session.check_remarks = request.POST['session_remarks']
-        session.check_date = timezone.now()
-        session.is_session_submitted = 1
-        session.save()
-
-        return HttpResponseRedirect(
-            reverse('srac:session.view', kwargs={'session_id': session.session_id}) + "?success=true"
+    location_checklists_set = Location_Checklist.objects.filter(location=location)
+    for location_checklist in location_checklists_set:
+        session.session_checklist_set.create(
+            session=session,
+            location_checklist=location_checklist
         )
-    else:
-        session = Session(
-            check_date=timezone.now(),
-            checker=current_user
-        )
-        session.save()
-        context['session'] = session
 
-        location_checklists_set = Location_Checklist.objects.filter(location=location)
-        for checklist in location_checklists_set:
-            session.session_checklist_set.create(
-                session=session,
-                location_checklist=checklist
-            )
+    session_checklist_set = session.session_checklist_set.all()
+    context['checklist_set'] = session_checklist_set
 
-        session_checklist_set = Session_Checklist.objects.filter(session=session)
-        context['checklist_set'] = session_checklist_set
-
-        return render(request, 'srac/checklist.html', context)
+    return render(request, 'srac/checklist.html', context)
 
 
 def session_edit(request, session_id):
@@ -131,8 +96,7 @@ def session_edit(request, session_id):
     session = Session.objects.get(pk=session_id)
     context['session'] = session
 
-    session_checklist_set = Session_Checklist.objects.filter(session=session)
-    context['checklist_set'] = session_checklist_set
+    context['checklist_set'] = session.session_checklist_set.all()
 
     return render(request, 'srac/checklist.html', context)
 
@@ -140,10 +104,9 @@ def session_edit(request, session_id):
 def session_save(request, session_id):
     if request.method == 'POST':
         session = Session.objects.get(pk=session_id)
-        session_checklist_set = Session_Checklist.objects.filter(session=session)
 
         # save changes
-        for session_checklist in session_checklist_set:
+        for session_checklist in session.session_checklist_set.all():
             session_checklist.confirmation = request.POST['confirmation_' + str(session_checklist.session_checklist_id)]
             session_checklist.remarks = request.POST['remarks_' + str(session_checklist.session_checklist_id)]
             session_checklist.save()
@@ -156,3 +119,15 @@ def session_save(request, session_id):
         return HttpResponseRedirect(
             reverse('srac:session.view', kwargs={'session_id': session.session_id}) + "?success=true"
         )
+
+
+def session_view(request, session_id):
+    session = get_object_or_404(Session, pk=session_id)
+    context = {
+        "checklist_set": session.session_checklist_set.all(),
+        "session": session
+    }
+    if "success" in request.GET:
+        context["success"] = "true"
+
+    return render(request, 'srac/checklist_view.html', context)
